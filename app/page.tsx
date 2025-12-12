5014d319b12f7d4b36318c423f535e466ac71726
alienthebetrayer <gpichkurov@gmail.com>
2025-12-12 17:48:18 +0200

    Initial Next.js commit + global variables

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export const Home = () => {
+	return (
+        <main className='w-full h-full max-w-80 min-h-48 m-auto bg-background-2 rounded-xl p-2'>
+        </main>
+	);
+};
+
+export default Home;
