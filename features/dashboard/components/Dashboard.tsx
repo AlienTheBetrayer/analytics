77b875ddca6b51ca28ccbd6387faa9e6bd054855
alienthebetrayer <gpichkurov@gmail.com>
2025-12-13 22:22:38 +0200

    Dashboard visibility state + recoverer fully done + animations better

--- features/dashboard/components/Dashboard.tsx
+++ features/dashboard/components/Dashboard.tsx
@@ -1,13 +1,13 @@
 "use client";
 import { useNotification } from "@/features/notification/hooks/useNotification";
-import { motion } from "motion/react";
-import { Activity, type ComponentPropsWithoutRef } from "react";
+import { usePopup } from "@/hooks/usePopup";
+import { AnimatePresence, motion } from "motion/react";
+import { type ComponentPropsWithoutRef, useEffect } from "react";
 import { useDashboardContext } from "../context/DashboardContext";
 import { useData } from "../hooks/useData";
+import { DashboardRecover } from "./additional/DashboardRecoverer";
 import { DashboardMain } from "./DashboardMain";
 import { DashboardTopline } from "./DashboardTopline";
-import { DashboardRecover } from "./additional/DashboardRecoverer";
-import { usePopup } from "@/hooks/usePopup";
 
 type Props = {} & ComponentPropsWithoutRef<"div">;
 
@@ -29,24 +29,37 @@ export const Dashboard = ({ className }: Props) => {
 	});
 
 	// state
-	const [state] = useDashboardContext();
+	const [state, dispatch] = useDashboardContext();
 
-    const recovererPopup = usePopup();
+	const recovererPopup = usePopup(
+		<DashboardRecover
+			onInteract={() => {
+				dispatch({ type: "SET_VISIBLE", flag: true });
+			}}
+		/>,
+	);
+
+	useEffect(() => {
+		recovererPopup.setIsShown(!state.visible);
+	}, [state.visible, recovererPopup.setIsShown]);
 
 	return (
 		<>
-            <DashboardRecover/>
-			<Activity mode={state.visible === true ? "visible" : "hidden"}>
-				<motion.div
-					initial={{ opacity: 0, y: 10 }}
-					animate={{ opacity: 1, y: 0 }}
-					className={`w-full h-full flex flex-col max-w-lg rounded-xl bg-linear-to-bl from-background-2 to-background-1 ${className ?? ""} hover:scale-105 duration-300`}
-				>
-					{notifications.render()}
-					<DashboardTopline controller={controller} />
-					<DashboardMain controller={controller} />
-				</motion.div>
-			</Activity>
+			{recovererPopup.render()}
+			<AnimatePresence>
+				{state.visible && (
+					<motion.div
+						initial={{ opacity: 0, y: 10 }}
+						animate={{ opacity: 1, y: 0 }}
+						exit={{ opacity: 0, y: 10 }}
+						className={`w-full h-full flex flex-col max-w-lg rounded-xl bg-linear-to-bl from-background-2 to-background-1 ${className ?? ""} hover:scale-105 duration-300`}
+					>
+						{notifications.render()}
+						<DashboardTopline controller={controller} />
+						<DashboardMain controller={controller} />
+					</motion.div>
+				)}
+			</AnimatePresence>
 		</>
 	);
 };
--- features/dashboard/components/additional/DashboardRecoverer.tsx
+++ features/dashboard/components/additional/DashboardRecoverer.tsx
@@ -1,10 +1,29 @@
+import { Button } from "@/features/ui/button/components/Button";
 import { motion } from "motion/react";
+import Image from "next/image";
+import eyeImg from "../../../../public/eye.svg";
 
-export const DashboardRecover = () => {
+type Props = {
+	onInteract: () => void;
+};
+
+export const DashboardRecover = ({ onInteract }: Props) => {
 	return (
 		<motion.div
-			className="flex flex-col fixed top-4 left-4 rounded-xl bg-linear-to-bl from-background-2 to-background-1 z-10"
-			initial={{ opacity: 0 }}
-			animate={{ opacity: 1 }}/>
+			className="flex items-center justify-between p-2 fixed top-4 left-4 rounded-xl bg-linear-to-bl 
+            from-background-2 to-background-1 z-10 gap-2 outline-1 outline-background-5 w-64"
+			initial={{ opacity: 0, y: -30 }}
+			animate={{ opacity: 1, y: 0 }}
+			exit={{ opacity: 0, y: -30 }}
+			transition={{ ease: "backInOut", duration: 0.5 }}
+		>
+			<span className="flex gap-1 items-center">
+				<Image className="image" alt="" src={eyeImg} />
+				Dashboard is <u>hidden</u>
+			</span>
+			<Button onClick={onInteract}>
+				<mark>Show</mark>
+			</Button>
+		</motion.div>
 	);
 };
