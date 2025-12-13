0df3753dd3f9729a04b925d98f0f345e0e4eac9b
alienthebetrayer <gpichkurov@gmail.com>
2025-12-13 22:26:54 +0200

    Events go back button done

--- features/dashboard/components/events/DashboardEvents.tsx
+++ features/dashboard/components/events/DashboardEvents.tsx
@@ -1,7 +1,6 @@
+import { Button } from "@/features/ui/button/components/Button";
 import type { useData } from "../../hooks/useData";
 import { DashboardEvent } from "./DashboardEvent";
-
-
 
 type Props = {
 	controller: ReturnType<typeof useData>;
@@ -19,9 +18,18 @@ export const DashboardEvents = ({ controller }: Props) => {
 		<div className="flex flex-col gap-4">
 			{projectData.metaData.length > 0 ? (
 				<>
-					<h3 className="text-center">
-						Events (<mark>{projectData.project.name}</mark>)
-					</h3>
+					<div className="flex relative min-h-8">
+						<h3 className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+							Events (<mark>{projectData.project.name}</mark>)
+						</h3>
+
+						<Button
+							className="ml-auto"
+							onClick={() => controller.setSelectedProjectId(null)}
+						>
+							<small>✕ Go back</small>
+						</Button>
+					</div>
 					<ul className="flex flex-col gap-2">
 						{projectData.metaData.map((metaDataEntry) => (
 							<DashboardEvent event={metaDataEntry} key={metaDataEntry.id} />
