f0645bfb6a33257c8f988563661c4debbda85749
alienthebetrayer <gpichkurov@gmail.com>
2025-12-16 01:26:53 +0200

    Button UI improvements

--- features/ui/button/components/Button.tsx
+++ features/ui/button/components/Button.tsx
@@ -5,9 +5,9 @@ type Props = {} & HTMLMotionProps<"button">;
 export const Button = ({ children, onClick, className, ...rest }: Props) => {
 	return (
 		<motion.button
-        onClick={onClick}
-        className={`flex gap-1 items-center justify-center rounded-3xl outline-1 outline-background-5 bg-background-3 min-w-8 min-h-8 px-2 hover:bg-background-4 active:bg-background-5 transition duration-150 cursor-pointer ${className ?? ""} `}
-        {...rest}
+			onClick={onClick}
+			className={`flex gap-1 items-center justify-center rounded-3xl outline-2 outline-background-5 bg-background-3 min-w-8 min-h-8 px-2 hover:brightness-150 active:brightness-200 focus-visible:outline-primary-1 transition duration-150 cursor-pointer ${className ?? ""} `}
+			{...rest}
 		>
 			{children}
 		</motion.button>
