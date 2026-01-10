65e56083ccdc61fa96c64b2701c3dea2368f0328
alienthebetrayer <gpichkurov@gmail.com>
2026-01-10 19:40:55 +0200

    Update Preferences.tsx

--- src/features/notifications/components/tabs/Preferences.tsx
+++ src/features/notifications/components/tabs/Preferences.tsx
@@ -1,10 +1,62 @@
+import { Tooltip } from "@/features/tooltip/components/Tooltip";
+import { Button } from "@/features/ui/button/components/Button";
 import { Topline } from "../toplines/preferences/Topline";
+import Image from "next/image";
+import { useState } from "react";
+import { Checkbox } from "@/features/ui/checkbox/components/Checkbox";
 
 export const Preferences = () => {
+    // react states
+    const [visibility, setVisibility] = useState<boolean>(true);
+
     return (
-        <div className="flex flex-col gap-2">
+        <div className="flex flex-col gap-2 grow">
             <Topline />
-            <hr/>
+            <hr />
+            <form
+                className="flex flex-col gap-2 grow"
+                onSubmit={(e) => {
+                    e.preventDefault();
+                }}
+            >
+                <label
+                    htmlFor="visibility"
+                    className="flex whitespace-nowrap"
+                >
+                    <span>Popups&apos; visiblity</span>
+                    <small className="ml-auto text-ellipsis-left">
+                        (whether to show them when they occur)
+                    </small>
+                </label>
+                <Checkbox
+                    id="visibility"
+                    value={visibility}
+                    onToggle={(f) => setVisibility(f)}
+                >
+                    Visibility
+                </Checkbox>
+                <hr className="mb-2" />
+
+                <hr className="mt-auto" />
+                <Tooltip
+                    className="w-full"
+                    direction="top"
+                    text="Apply the preferences' changes"
+                >
+                    <Button
+                        type="submit"
+                        className="w-full"
+                    >
+                        <Image
+                            alt=""
+                            width={12}
+                            height={12}
+                            src="/checkmark.svg"
+                        />
+                        Apply changes
+                    </Button>
+                </Tooltip>
+            </form>
         </div>
     );
 };
