c6069c09ceda8f13cb36ef08fb758b91bbcb51ee
alienthebetrayer <gpichkurov@gmail.com>
2026-01-18 13:09:09 +0200

    Animated promise status

--- src/features/spinner/components/Spinner.tsx
+++ src/features/spinner/components/Spinner.tsx
@@ -4,20 +4,26 @@ import "./Spinner.css";
 import { SpinnerStyles } from "../utils/styles";
 
 type Props = {
-	styles?: keyof typeof SpinnerStyles;
+    styles?: keyof typeof SpinnerStyles;
     width?: number;
     height?: number;
-} & Omit<ComponentPropsWithoutRef<"img">, 'src' | 'width' | 'height'>;
+} & Omit<ComponentPropsWithoutRef<"img">, "src" | "width" | "height">;
 
-export const Spinner = ({ className, styles, width, height, ...rest }: Props) => {
-	return (
-		<Image
-			alt="Loading"
-			src="/spinner.svg"
-			width={width ?? 16}
-			height={height ?? 16}
-			className={`loading-spinner ${className ?? ""} ${styles ? SpinnerStyles[styles] : ""}`}
+export const Spinner = ({
+    className,
+    styles,
+    width,
+    height,
+    ...rest
+}: Props) => {
+    return (
+        <Image
+            alt="Loading"
+            src="/spinner.svg"
+            width={width ?? 16}
+            height={height ?? 16}
+            className={`loading-spinner ${className ?? ""} ${styles ? SpinnerStyles[styles] : ""}`}
             {...rest}
-		/>
-	);
+        />
+    );
 };
