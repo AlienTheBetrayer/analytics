6809077a0cfc323bfee8069b97b019e46be1312f
alienthebetrayer <gpichkurov@gmail.com>
2026-02-07 22:16:17 +0200

    Theme button sun image

--- src/features/header/components/toolbox/components/Theme.tsx
+++ src/features/header/components/toolbox/components/Theme.tsx
@@ -19,7 +19,7 @@ export const Theme = ({ className, children }: Props) => {
         >
             <Image
                 alt="theme"
-                src="/moon.svg"
+                src={theme === "dark" ? "/moon.svg" : "/sun.svg"}
                 width={18}
                 height={18}
                 className="transition-all duration-1000!"
