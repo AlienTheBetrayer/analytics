21cf793dc00ec8eb1ad0fdbecefc3118a4f411b2
alienthebetrayer <gpichkurov@gmail.com>
2026-02-16 18:16:42 +0200

    /api/get/noteboard API

---
+++ src/app/api/get/noteboard/route.ts
@@ -0,0 +1,28 @@
+import { supabaseServer } from "@/server/private/supabase";
+import { nextResponse } from "@/utils/api/response";
+import { NextRequest } from "next/server";
+
+export const GET = async (request: NextRequest) => {
+    try {
+        const { searchParams } = request.nextUrl;
+        const user_id = searchParams.get("user_id");
+
+        if (!user_id) {
+            throw "user_id is undefined";
+        }
+
+        const { data, error } = await supabaseServer
+            .from("noteboards")
+            .select("*, elements:noteboard_elements(*)")
+            .eq("user_id", user_id);
+
+        if (error) {
+            throw error;
+        }
+
+        return nextResponse({ success: true, noteboards: data }, 200);
+    } catch (error) {
+        console.error(error);
+        return nextResponse({ success: false }, 400);
+    }
+};
---
+++ src/types/tables/notes.ts
@@ -0,0 +1,27 @@
+/**
+ * noteboards
+ */
+export type Noteboard = {
+    id: string;
+    user_id: string;
+    title: string;
+    description?: string;
+    pinned: boolean;
+    pinned_at?: boolean;
+    edited_at?: string;
+    created_at: string;
+}
+
+/**
+ * noteboard_elements
+ */
+export type NoteboardElement = {
+    id: string;
+    noteboard_id: string;
+    title: string;
+    checked: boolean;
+    pinned: boolean;
+    pinned_at?: string;
+    edited_at?: string;
+    created_at: string;
+}
