4de234e95723b80bef300cb518c59813a62ea69b
alienthebetrayer <gpichkurov@gmail.com>
2026-02-17 21:19:31 +0200

    UI and FPS improvements

--- src/features/ui/popovers/components/modal/Modal.tsx
+++ src/features/ui/popovers/components/modal/Modal.tsx
@@ -169,7 +169,7 @@ export const Modal = React.memo(function ModalFunction({
                                 className="fixed"
                             >
                                 <motion.div
-                                    className="backdrop-blur-lg rounded-4xl modal-element"
+                                    className="backdrop-blur-sm rounded-4xl modal-element"
                                     initial={{
                                         opacity: 0,
                                         scale: 0.9,
