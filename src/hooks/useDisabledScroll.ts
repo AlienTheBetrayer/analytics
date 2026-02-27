bbcb2348b8e9c24b5795c8bde3a34588045fa9d7
alienthebetrayer <gpichkurov@gmail.com>
2026-02-27 21:24:16 +0200

    useDisabledScroll() fix

--- src/hooks/useDisabledScroll.ts
+++ src/hooks/useDisabledScroll.ts
@@ -1,42 +1,22 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect, useMemo } from "react";
 
 export const useDisabledScroll = () => {
     const [isDisabled, setIsDisabled] = useState<boolean>(false);
 
     useEffect(() => {
-        if (isDisabled === true) {
-            const prevent = (e: Event) => {
-                e.preventDefault();
-            };
-
-            const preventKeys = (e: KeyboardEvent) => {
-                const keys = [
-                    "ArrowUp",
-                    "ArrowDown",
-                    "PageUp",
-                    "PageDown",
-                    "Space",
-                    "Home",
-                    "End",
-                ];
-                if (keys.includes(e.code)) {
-                    e.preventDefault();
-                }
-            };
-
-            window.addEventListener("wheel", prevent, { passive: false });
-            window.addEventListener("touchmove", prevent, { passive: false });
-            window.addEventListener("keydown", preventKeys, { passive: false });
+        if (isDisabled) {
+            const originalStyle = window.getComputedStyle(
+                document.body,
+            ).overflow;
+            document.body.style.overflow = "hidden";
 
             return () => {
-                window.removeEventListener("wheel", prevent);
-                window.removeEventListener("touchmove", prevent);
-                window.removeEventListener("keydown", preventKeys);
+                document.body.style.overflow = originalStyle;
             };
         }
     }, [isDisabled]);
 
-    return {
-        setIsDisabled,
-    };
+    return useMemo(() => {
+        return { setIsDisabled };
+    }, [setIsDisabled]);
 };
